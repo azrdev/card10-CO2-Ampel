507ae9e43665f968f647096b4d3e4ed6676e2830
azrdev <azrdev@qrdn.de>
2022-02-05 14:32:44 +0100

    app c10vid → Ampel

--- __init__.py
+++ __init__.py
@@ -6,23 +6,33 @@ import vibra
 import buttons
 
 
-colors = {'lime': [0, 255, 0], 'green': [0, 128, 0], 'yellow': [255, 255, 0], 'orange': [
-    255, 128, 0], 'red': [255, 0, 0], 'purple': [128, 0, 128], 'brown': [128, 0, 0], 'blue': [0, 0, 255]}
+IAQ_STEP = 25  # all 11 LEDs ~= iaq 300, offset 25 (first always on), below that linear
 
-gs = 160
-all_colors = [((i >> 2) * gs, (i >> 1 & 1) * gs, (i & 1) * gs)
-              for i in range(1, 8)]
+# iaq_accuracy thresholds
+# see https://firmware.card10.badge.events.ccc.de/epicardium/api.html#bsec-api
+IAQ_ACCU_LOW = 1
+IAQ_ACCU_MEDIUM = 2
+IAQ_ACCU_HIGH = 3
 
-_rand = 123456789
+# rocket constants
+ROCKET_BLUE = 0  # Left
+ROCKET_YELLOW = 1  # Top
+ROCKET_GREEN = 2  # Right
 
-
-def rand():
-    global _rand
-    _rand = (1103515245 * _rand + 12345) & 0xFFFFFF
-    return _rand
+colors = {
+  'lime': [0, 255, 0],
+  'green': [0, 128, 0],
+  'yellow': [255, 255, 0],
+  'orange': [255, 128, 0],
+  'red': [255, 0, 0],
+  'purple': [128, 0, 128],
+  'brown': [128, 0, 0],
+  'blue': [0, 0, 255]
+}
 
 
 def iaq_color(iaq):
+    """Map color from iaq value as defined in BME680 datasheet"""
     if (iaq < 50):
         return colors['lime']
     if (iaq < 100):
@@ -56,109 +66,94 @@ def iaq_string(iaq):
         return "Leave!"
 
 
-def leds_set_bottom(co2_color):
-    leds.set(11, co2_color)
-    leds.set(12, co2_color)
-    leds.set(13, co2_color)
-    leds.set(14, co2_color)
+def accuracy_string(iaq_accuracy):
+    if iaq_accuracy < IAQ_ACCU_LOW:
+        return "Run-in"
+    elif iaq_accuracy < IAQ_ACCU_MEDIUM:
+        return "Low"
+    elif iaq_accuracy < IAQ_ACCU_HIGH:
+        return "Medium"
+    else:
+        return "High"
 
 
 def main():
 
-    power_saving = False
+    power_saving = False  # toggled by button. True → screen off
 
     disp = display.open()
     disp.backlight(25)
     disp.clear().update()
     leds.clear()
-    leds.set_powersave()
+    leds.set_powersave(True)
     leds.dim_top(1)
     leds.dim_bottom(1)
 
+    # main loop
+    tick = True
     with bme680.Bme680() as environment:
         while True:
-            data = environment.get_data()
+            tick = not tick
 
-            disp.clear()
+            data = environment.get_data()
 
             # button toggle screen
             if buttons.read(buttons.TOP_RIGHT):
                 power_saving = not power_saving
 
-            if(data.iaq_accuracy >= 2):
+            # set 11 top row LEDs according to IAQ
+            iaq_color_now = iaq_color(data.iaq)
+            on_led_count = min(data.iaq // IAQ_STEP + 1, 11*IAQ_STEP)
+            for i in range(11):
+                leds.prep(i,
+                          iaq_color_now if i < on_led_count
+                          else [0, 0, 0])
 
-                leds.set_all([iaq_color(data.iaq)]*11)
+            # set 4 bottom LEDs according to eCO2
+            if data.eco2 < 600:
+                co2_color = colors['green']
+            elif data.eco2 < 900:
+                co2_color = colors['orange']
+            else:  # eco2 >= 900
+                co2_color = colors['red']
+            leds.prep(leds.BOTTOM_LEFT, co2_color)
+            leds.prep(leds.BOTTOM_RIGHT, co2_color)
+            leds.prep(leds.TOP_RIGHT, co2_color)
+            leds.prep(leds.TOP_LEFT, co2_color)
 
-                # Set green rocket if under 600 ppm: no mask required
-                if(data.eco2 < 600):
-                    co2_text = "No masks required"
-                    co2_color = colors['green']
-                    leds.set_rocket(2, 31)
-                    leds.set_rocket(1, 0)
-                    leds.set_rocket(0, 0)
-
-                # Set blue rocket if over 600 ppm: masks required
-                elif(data.eco2 >= 600 and data.eco2 < 900):
-                    co2_text = "Wear masks"
-                    co2_color = colors['blue']
-                    leds.set_rocket(0, 31)
-                    leds.set_rocket(1, 0)
-                    leds.set_rocket(2, 0)
-                # Set yellow rocket if over 900 ppm: dangerous even with masks on
+            leds.set_rocket(ROCKET_BLUE, 0)
+            if data.iaq_accuracy < IAQ_ACCU_MEDIUM:  # reading is bad
+                # blue rocket always on
+                leds.set_rocket(ROCKET_BLUE, 12)
+            elif data.iaq_accuracy < IAQ_ACCU_HIGH:  # reading could be more accuate
+                # blue rocket blinking
+                if tick:
+                    leds.set_rocket(ROCKET_BLUE, 12)
                 else:
-                    co2_text = "Masks could be not enough!"
-                    co2_color = colors['orange']
+                    leds.set_rocket(ROCKET_BLUE, 0)
 
-                    leds.set_rocket(1, 31)
-                    leds.set_rocket(2, 0)
-                    leds.set_rocket(0, 0)
-                #     vibra.vibrate(500)
-                #     time.sleep(1)
-                #     vibra.vibrate(500)
-                #     time.sleep(1)
-                #     vibra.vibrate(500)
+            leds.update()
 
-                leds_set_bottom(co2_color)
-
-                if(not power_saving):
-                    disp.backlight(25)
-                    disp.print("IAQ: " + str(data.iaq))
-                    disp.print("CO2: " +
-                               str(int(data.eco2)) + "ppm", posy=40)
-                    if(data.iaq_accuracy == 3):
-                        disp.print(iaq_string(data.iaq), posy=20,
-                                   fg=iaq_color(data.iaq))
-                        disp.print(co2_text, posy=65, fg=co2_color,
-                                   font=display.FONT12)
-                    else:
-                        disp.print("(still calibrating...)",
-                                   posy=60, font=display.FONT12)
-                else:
-                    disp.clear()
-                    disp.backlight(0)
-
-                disp.update()
-
-                time.sleep(1)
+            disp.clear()
+            if(not power_saving):
+                disp.backlight(25)
+                # eCO2: 666          #
+                # IAQ: 323           #
+                #   accuracy: Medium #
+                disp.print("eCO2: " + str(int(data.eco2)) + "ppm")
+                disp.print("IAQ: " + str(data.iaq), posy=20)
+                disp.print("accuracy: " + accuracy_string(data.iaq_accuracy),
+                           fg=iaq_color_now, posy=40)
+                # if iaq_accuracy < HIGH, recalibration occurs.
+                # Place the badge both in open-air then in a closed box with exhaled air for around 10min each.
 
             else:
-                disp.print("Calibrating...", posy=0, font=display.FONT16)
-                disp.print(
-                    "Place the badge both in open-air then in a closed box with exhaled air for around 10min each.", posy=40, font=display.FONT8)
+                disp.backlight(0)
+                # TODO: dim LEDs
 
-                disp.update()
-                if(not power_saving):
-                    oldest_time = time.time()
-                    latest_time = oldest_time
-                    while (latest_time - oldest_time < 1):
-                        leds.set(rand() %
-                                 11, all_colors[rand() % len(all_colors)])
-                        time.sleep_ms(1)  # Feed watch doge
-                        latest_time = time.time()
+            disp.update()
 
-                else:
-                    leds.set_all([[0, 0, 0]]*15)
-                    time.sleep(1)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
